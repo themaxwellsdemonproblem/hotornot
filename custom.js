8951c35dcf63a7ff59d3b2cde86b53ac79f91dd9
Matthew <mmontesano@health.nyc.gov>
2021-02-23 10:20:40 -0500

    Additional basics

--- custom.js
+++ custom.js
@@ -1,12 +1,16 @@
 function showTrack(x) {
     for (let output of document.querySelectorAll('.holder')) output.classList.add('hide');
+    for (let output of document.querySelectorAll('.holder')) output.classList.remove('show');
 
     if (x === 1) {
-        document.getElementById('home').classList.remove('show')
         document.getElementById('tr1').classList.toggle('hide')
+        document.getElementById('tr1').classList.add('show')
+
+    } else if (x === 2) {
+        document.getElementById('tr2').classList.toggle('hide');
+        document.getElementById('tr2').classList.add('show')
     } else {
         document.getElementById('home').classList.add('show');
-        document.getElementById('home').classList.toggle('hide');
     };
 
 
